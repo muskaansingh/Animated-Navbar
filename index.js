61112c00461363d274f7126d2b4792d446129648
Muskaan Singh <47673038+muskaansingh@users.noreply.github.com>
2021-03-02 14:45:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const navbar = document.querySelector('.navbar');
+const navlinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+navbar.addEventListener("click", () =>{
+    navlinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle('fade')
+    });
+});
